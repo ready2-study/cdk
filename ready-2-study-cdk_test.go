26d1134aee1630d539f513e2e8e22d9e704f7dd4
Jakub-Ignatowicz <kuba_ign@proton.me>
2024-03-10 21:16:34 +0100

    Initial commit

---
+++ ready-2-study-cdk_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in ready-2-study-cdk_test.go
+// func TestReady2StudyCdkStack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewReady2StudyCdkStack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack, nil)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
